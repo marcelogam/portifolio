68b82ca59e14f48e248fc5bd5c538eceff704182
marceloGMA <marcelo.gomes@tcsindustrial.com.br>
2026-03-24 16:11:22 -0300

    Arrumando a questão das imagens

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  basePath: isProd ? '/portifolio' : '',
+  assetPrefix: isProd ? '/portifolio/' : '',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -6,7 +6,7 @@ import ProjectTag from "./ProjectTag";
 const projects = [
     {
         id: 1,
-        imgUrl: 'public/images/projects/bhPolaris.png',
+        imgUrl: '/images/projects/bhPolaris.png',
         title: 'Bh polaris',
         description: 'Description for project one.',
         tag: ["All", "Web"],
@@ -15,7 +15,7 @@ const projects = [
     },
     {
         id: 2,
-        imgUrl: 'public/images/projects/ye7.png',
+        imgUrl: '/images/projects/ye7.png',
         title: 'Ye7',
         description: 'Description for project two.',
         tag: ["All", "Mobile"],
